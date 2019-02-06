22b0797c08c9e1a70179cc7f8b03b00bcb3bb119
Juanmi Alberola <jmalberola@florida-uni.es>
2019-02-06 12:17:00 +0100

    ejemplos refuerzo

---
+++ ejemploLiga_v3/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="equipos.php">Equipos</a></li>
+            <li><a href="jugadores.php">Jugadores</a></li>
+            <li><a href="#">#</a></li>
+          </ul>
+        </div>
+      </nav>
+
+    </div>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
---
+++ ejemploLiga_v3/jugadores.php
@@ -0,0 +1,50 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "liga");
+if ($conexion->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+}else{
+  $resultado = $conexion->query("SELECT * FROM jugador");
+}
+ ?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+      <nav>
+        <div class="nav-wrapper">
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="#">#</a></li>
+          </ul>
+        </div>
+      </nav>
+      <h2>Jugador</h2>
+      <table>
+        <tr>
+          <th>id</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+        </tr>
+        <?php
+          foreach ($resultado as $jugador) {
+            echo "<tr>";
+            echo "<td>".$jugador['id_jugador']."</td>";
+            echo "<td>".$jugador['nombre']."</td>";
+            echo "<td>".$jugador['apellido']."</td>";
+            echo "</tr>";
+          }
+        ?>
+      </table>
+
+    </div>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
